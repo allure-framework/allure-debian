7accee702a1c072bfa43e604d2d2e7457c745708
Dmitry Baev <baev.dm@gmail.com>
2015-10-27 19:27:47 +0300

    update to 1.4.19

--- source-prepare.sh
+++ source-prepare.sh
@@ -5,5 +5,5 @@ fi
 if [[ -f allure-commandline.zip ]]; then
     rm allure-commandline.zip
 fi
-wget https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.18/allure-commandline.zip
+wget https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.19/allure-commandline.zip
 unzip allure-commandline.zip -d allure-commandline
