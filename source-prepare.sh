5efdb37ac4a0084dec388d3d91d7649dc2fdec5f
Artem Eroshenko <eroshenkoam@qameta.io>
2017-08-28 16:55:43 +0300

    update to allure 2

--- source-prepare.sh
+++ source-prepare.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-if [[ -d allure-commandline ]]; then
-    rm -Rf allure-commandline
+
+WORK_DIR=target
+ALLURE_VERSION=2.3.3
+
+if [[ ! -d ${WORK_DIR} ]]; then
+    mkdir ${WORK_DIR}
 fi
-if [[ -f allure-commandline.zip ]]; then
-    rm allure-commandline.zip
+
+if [[ -d ${WORK_DIR}/allure ]]; then
+    rm -Rf ${WORK_DIR}/allure
 fi
-wget https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.23.HOTFIX1/allure-commandline.zip
-unzip allure-commandline.zip -d allure-commandline
+
+if [[ -f ${WORK_DIR}/allure.zip ]]; then
+    rm ${WORK_DIR}/allure.zip
+fi
+
+wget "https://dl.bintray.com/qameta/generic/io/qameta/allure/allure/${ALLURE_VERSION}/allure-${ALLURE_VERSION}.zip" \
+     -O "${WORK_DIR}/allure.zip"
+
+unzip "${WORK_DIR}/allure.zip" -d "${WORK_DIR}"
+mv "${WORK_DIR}/allure-${ALLURE_VERSION}" "${WORK_DIR}/allure"
